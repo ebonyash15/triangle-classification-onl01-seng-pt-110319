3de669743e7f151d294e2b8603729f233d6133d0
Ebony Sharrock <ebonysharrock@gmail.com>
2019-12-16 03:56:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,6 +4,14 @@ class Triangle
     @length1=length1
     @length2=length2
     @length3=length3
+    if (@length1>0&&@length2>0&&@length3>0)&&(@length1+@length2>@length3)
+    else
+      begin
+        raise TrialngleError
+      rescue TrialngleError => error
+        puts error.message
+      end
+    end
   end
   class TriangleError < StandardError
     def message
@@ -11,20 +19,12 @@ class Triangle
     end
   end
   def kind
-    if (@length1>0&&@length2>0&&@length3>0)&&(@length1+@length2>@length3)
-      if @length1==@length2&&@length2==@length3
-        :equilateral
-      elsif @length1==@length2 || @length2==@length3 || @length1==@length3
-        :isosceles
-      else
-        :scalene
-      end
+    if @length1==@length2&&@length2==@length3
+      :equilateral
+    elsif @length1==@length2 || @length2==@length3 || @length1==@length3
+      :isosceles
     else
-      begin
-        raise TrialngleError
-      rescue TrialngleError => error
-        puts error.message
-      end
+      :scalene
     end
   end
 end

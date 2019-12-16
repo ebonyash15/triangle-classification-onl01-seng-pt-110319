228254101628b19974157fa9d2da119f0fa8a492
Ebony Sharrock <ebonysharrock@gmail.com>
2019-12-16 03:52:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
     if (@length1>0&&@length2>0&&@length3>0)&&(@length1+@length2>@length3)
       if @length1==@length2 && @length2=@length3
         :equilateral
-      elsif @length1==@length2 || @length2==@length3
+      elsif @length1==@length3
         :isosceles
       else
         :scalene
